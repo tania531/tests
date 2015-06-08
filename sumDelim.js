bae4e4d6e6c7f39ecbe841d58a8d5b2da136aff5
Tania <tania.dev77@gmail.com>
2015-06-08 10:31:00 -0700

    add sum delim

---
+++ sumDelim.js
@@ -0,0 +1,23 @@
+
+
+// 2. splitSum()
+// Takes 2 arguments
+//--------------------------------------------------------
+result = splitSum("3:4:5:1", ":");
+console.log(result); // 13
+result = splitSum("-1$-5$5$-4", "$");
+console.log(result); // -5
+
+function splitSum(string, delim){
+  console.log("arguments: ", arguments);
+  delim = arguments[1];
+  string = string.split(""+delim+"");
+  string.forEach(function(s){
+    s = s*1;
+  });
+  console.log("string: ", string);
+  console.log("delim: ", delim);
+  string.reduce(function(acc, curr){
+    return acc + curr;
+  });
+}
